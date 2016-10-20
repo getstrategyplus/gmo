20c2d45e2472efbdf8a4aeb1269b8a02f0828870
Gustavo Honorato <gustavo.honorato@seomaster.com.br>
2016-10-20 14:50:14 -0200

    change routes to make root_path accept attributes in path (ex. /23-10-2016)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'newsletter#show'
+  root to: 'newsletters#show'
+  get ':date', to: 'newsletters#show', as: :newsletter_show
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
