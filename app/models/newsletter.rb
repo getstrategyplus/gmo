12708287f360904305e3642316086d4bdb046c35
Gustavo Honorato <gustavo.honorato@seomaster.com.br>
2016-10-20 14:49:10 -0200

    adding scopes on models

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,2 +1,6 @@
 class Newsletter < ApplicationRecord
+  has_many :news
+
+  scope :sent_at_date, -> (date) { where(sent_at: date) }
+  scope :next_sent_date, -> (date) { where('sent_at > ?', date).minimum(:sent_at) }
 end
