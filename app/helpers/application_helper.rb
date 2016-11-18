2e66070a979b5f3f0e056da69071fc0abbed3a46
Gustavo Honorato <gustavo.honorato@seomaster.com.br>
2016-11-18 16:56:28 -0200

    implementing title correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def page_title
+    content_tag :title do 
+      content_for?(:title) ? "#{content_for(:title)} - Strategy+" : 'A simple newsletter made by experts to understand global politics - Strategy+'
+    end
+  end
+
+  def page_meta_description
+    tag :meta, name: 'description', content: (content_for?(:meta_description) ? content_for(:meta_description) : 'Strategy+ is the smartest way to follow various topics about the global political agenda. Receive every day a small email to receive a briefing about key geopolitical issues.')    
+  end
 end
