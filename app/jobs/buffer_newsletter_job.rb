6772759dfcfce7ea74b3f3c2490875dec1f4d600
Gustavo Honorato <gustavo.honorato@seomaster.com.br>
2016-11-20 20:58:52 -0200

    correcting content sent to buffer

--- app/jobs/buffer_newsletter_job.rb
+++ app/jobs/buffer_newsletter_job.rb
@@ -4,34 +4,34 @@ class BufferNewsletterJob < ApplicationJob
  
   def perform(id)
     newsletter = Newsletter.find(id)
-    url = Rails.application.routes.url_helpers.newsletter_show_url(date: newsletter.sent_at, host: 'getstrategyplus.com')
+    url = Rails.application.routes.url_helpers.newsletter_show_url(date: newsletter.sent_at.strftime("%d-%m-%Y"), host: 'getstrategyplus.com')
 
     client = Buffer::Client.new(Rails.application.secrets.buffer_api_key)
 
     create_update(client, 
       text: "#{newsletter.title} - #{url}", 
       image: newsletter.news.first.image, 
-      services: ['twitter']
+      services: ['twitter', 'instagram']
     )
 
     create_update(client, 
       text: "#{newsletter.title} - #{newsletter.excerpt} - #{url}", 
       image: newsletter.news.first.image,
-      services: ['facebook', 'instagram']
+      services: ['facebook']
     )
 
     unless newsletter.sent_at.sunday? || newsletter.sent_at.saturday?
       newsletter.news.each do |n|
         create_update(client, 
-          text: "#{newsletter.title} - #{url}", 
+          text: "#{news.title} - #{url}", 
           image: n.image, 
           services: ['twitter']
         )
 
         create_update(client, 
-          text: "#{newsletter.title} - #{newsletter.excerpt} - #{url}", 
+          text: "#{news.title} - #{news.excerpt} - #{url}", 
           image: n.image, 
-          services: ['facebook', 'instagram']
+          services: ['facebook']
         )
       end
     end
