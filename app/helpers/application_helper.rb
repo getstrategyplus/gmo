7b94a04a8f93576efe48e5c7c54b94107816fd56
Gustavo Honorato <gustavo.honorato@seomaster.com.br>
2017-01-25 11:05:07 -0200

    pivotal typos on title and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
   def page_title
     content_tag :title do 
-      content_for?(:title) ? "#{content_for(:title)} - Strategy+" : 'A simple newsletter made by experts to understand global politics - Strategy+'
+      content_for?(:title) ? "#{content_for(:title)} - Strategy+" : 'A newsletter to learn how to launch & run a great online business - Strategy+'
     end
   end
 
   def page_meta_description
-    description = content_for?(:meta_description) ? content_for(:meta_description) : 'Strategy+ is the smartest way to follow various topics about the global political agenda. Receive every day a small email to receive a briefing about key geopolitical issues.'
+    description = content_for?(:meta_description) ? content_for(:meta_description) : 'Strategy+ is the smartest way to keep track of awesome content regarding how to launch and operate a successful, global, digital business.'
     tag :meta, name: 'description', content: sanitize(truncate(description, length: 150))
   end
 end
