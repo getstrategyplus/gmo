556295c430909084f737a1c71906eea81c53b1e6
Gustavo <gustavo@MacBook-Pro-de-Mac.local>
2017-10-09 11:43:13 -0300

    Fixing more typos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
   def page_title
     content_tag :title do
-      content_for?(:title) ? "#{content_for(:title)} - Startup Cuts" : 'A newsletter to learn how to launch & run a great online business - Startup Cuts'
+      content_for?(:title) ? "#{content_for(:title)} - Startup Cuts Plus" : 'A newsletter to learn how to launch & run a great online business - Startup Cuts Plus'
     end
   end
 
   def page_meta_description
-    description = content_for?(:meta_description) ? content_for(:meta_description) : 'Startup Cuts is the smartest way to keep track of awesome content regarding how to launch and operate a successful, global, digital business.'
+    description = content_for?(:meta_description) ? content_for(:meta_description) : 'Startup Cuts Plus is the smartest way to keep track of awesome content regarding how to launch and operate a successful, global, digital business.'
     tag :meta, name: 'description', content: sanitize(truncate(description, length: 150))
   end
 end
